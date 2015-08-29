80c49d666712782de4bf827b7df9baa00aac9938
iamcap <ethan@lib-dhcp-155.ci.mtnview.ca.us>
2015-08-28 17:43:15 -0700

    Initial Setup for App
    Initial setup for app

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nasInFinderApp
+ * @description
+ * # nasInFinderApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nasInFinderApp', [
+    'ngCookies'
+  ]);
---
+++ app/scripts/controllers/list.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nasInFinderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nasInFinderApp
+ */
+angular.module('nasInFinderApp')
+  .controller('ListCtrl', function () {
+    this.awesomeThings = [
+    ];
+  });
